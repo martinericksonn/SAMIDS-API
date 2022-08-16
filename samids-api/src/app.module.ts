54a17ddc68f7e508276f9436678e1f79d2a5d46c
Martin <lapetajemartin@gmail.com>
2022-08-16 18:14:48 +0800

    add module accounts, attendance, and schedule

--- samids-api/src/app.module.ts
+++ samids-api/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { AttendanceModule } from './attendance/attendance.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, AttendanceModule, ScheduleModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
