b5539965d7de881df788c5472318be7e924eac02
Martin <lapetajemartin@gmail.com>
2022-09-18 02:33:01 +0800

    add express multer for image details

--- samids-api/src/image/image.controller.ts
+++ samids-api/src/image/image.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('image')
-export class ImageController {}
+export class ImageController {
+  @Post('/add')
+  @UseGuards(AuthGuard('api-key'))
+  @UseInterceptors(FileInterceptor('file'))
+  addAttendance(@Body() body: Express.Multer.File) {
+    return 'File received';
+  }
+}
