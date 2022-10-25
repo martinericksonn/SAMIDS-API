3a7109582d67b4ca6208c45da50e03e0162d7784
Martin <lapetajemartin@gmail.com>
2022-10-25 23:37:33 +0800

    image controller multer

--- samids-api/src/image/image.controller.ts
+++ samids-api/src/image/image.controller.ts
@@ -1,7 +1,10 @@
 import {
   Body,
   Controller,
+  HttpStatus,
+  ParseFilePipeBuilder,
   Post,
+  UploadedFile,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
@@ -10,10 +13,14 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('image')
 export class ImageController {
-  @Post('/add')
+  @Post('upload')
   @UseGuards(AuthGuard('api-key'))
   @UseInterceptors(FileInterceptor('file'))
-  addAttendance(@Body() body: Express.Multer.File) {
+  uploadImage(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    console.log('file', file);
     return 'File received';
   }
 }
